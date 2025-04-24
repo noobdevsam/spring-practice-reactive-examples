c5e983c7756771e1bd1e1cb010cb4186ec3b1e14
noobdevsam <mdshoykotislam05@gmail.com>
2025-04-25 01:01:37 +0600

    add sample Person instances to PersonRepositoryImpl

--- src/main/java/com/example/springpracticereactiveexamples/repositories/PersonRepositoryImpl.java
+++ src/main/java/com/example/springpracticereactiveexamples/repositories/PersonRepositoryImpl.java
@@ -5,6 +5,12 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PersonRepositoryImpl implements PersonRepository {
+
+    Person michael = new Person(1, "Michael", "Jordan");
+    Person john = new Person(2, "John", "Doe");
+    Person jane = new Person(3, "Jane", "Doe");
+    Person alice = new Person(4, "Alice", "Doe");
+
     @Override
     public Mono<Person> findById(Integer id) {
         return null;
